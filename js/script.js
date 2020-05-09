e49f5544295ddc0cb28605aa3b83b381e56772ea
Mariomnue <mary.pienzi@gmail.com>
2020-05-09 12:22:26 -0400

    Working script.js Need to test in other browsers go through QA Then work on EC

--- js/script.js
+++ js/script.js
@@ -3,134 +3,54 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
+//showPage displays the truncated list based on the page number sent to it.
+//it uses the list argument to generate the list of students to display.
 const student_list = document.getElementsByClassName("student-item cf");
 const number_of_items = 10;
-//const student-details = student_list[0].firstElementChild.innerHTML
-//const joined-details = student_list[0].lastElementChild.innerHTML
-
-//console.log("student_list length: " +student_list.length);
-
 const showPage = (list, page) => {
 list = student_list;//remove me later
   let startIndex = (page * number_of_items) - number_of_items;
   let endIndex = page * number_of_items;
-//console.log(startIndex+ "    " +endIndex);
-  for(var i=0; i<student_list.length; i++){
-    student_list[i].style.display = "none"
+  for(var i=0; i<list.length; i++){
+    list[i].style.display = "none"
     if(i >= startIndex && i < endIndex){
-      student_list[i].style.display = "block"
+      list[i].style.display = "block"
     }
   }
-  appendPageLinks(list, 1);
 }
 
 showPage(1, 1);///never send a 0;
-//pagination(1);
-function appendPageLinks(list, page){
-//look for the list below before you recreate it!
-// if(div !== true){
-//   console.log("true")
-// }
-// let startIndex = (page * number_of_items) - number_of_items;
-// let endIndex = page * number_of_items;
-// let newList;
-// //console.log(newList.NodeType);
-// for(var i=startIndex; i<10; i++){
-// console.log(list)
-//   newList.push(list[i]);
-// }
+appendPageLinks(1, 1);
 
-let page_list = student_list;///student_list will be subst with jsut: list
+//the appendPageLinks function builds the navigation buttons at the bottom.
+function appendPageLinks(list, page){
+  let startIndex = (page * number_of_items) - number_of_items;
+  let endIndex = page * number_of_items;
+  let page_list = student_list;///student_list will be subst with jsut: list
     const div = document.createElement('div')
     div.className = "pagination"
-    list = document.querySelector('.page')
+    list = document.querySelector('.page')//isn't this the same as student_list ?
     list.appendChild(div)
     const ul = document.createElement('ul');
     div.appendChild(ul);
-//console.log(div.parentNode);
-
     let numPages = Math.ceil(page_list.length / number_of_items)
-
-
     for(let i=1; i<=numPages; i++){
       let li = document.createElement('li');
       let span = document.createElement('span');////remove
-//console.log(li.parentNode)//there is no parent at this point.
-
-      if(list === i){
-        span.innerHTML = '<a class="active" href="#">'+i+'</a>';
-        li.addEventListener('click', (e) =>{
-            //showPage(1, 2);
-
-        })
-      }
-      else{
-        span.innerHTML = '<a href="#">'+i+'</a>';//appendPageLinks
-        //let textStuff = '<a href="#">'+i+'</a>';
-//console.log(li.NodeType)
-        //li.appendPageLinks('<a href="#">'+i+'</a>')
-        ul.appendChild(li);
-        li.appendChild(span);
-        ul.appendChild(li);
-      //ul.appendChild(li);
-      }
-      //span.innerHTML = '<a class="active" href="#">'+i+'</a>';
-
-      ul.addEventListener('click', (e) =>{
-console.log(e+ ' do something ' +e);
-        //showPage(1, 2);
-///          alert(e+ '  ' +e);
-      })
+        if(i === page){
+          span.innerHTML = '<a class="active" href="#">'+i+'</a>';
+        }
+        else{
+          span.innerHTML = '<a href="#">'+i+'</a>';//appendPageLinks
+        }
+      li.appendChild(span);
+      ul.appendChild(li);
     }
-
-
-console.log(student_list.length+ " " +numPages)
+    ul.addEventListener('click', (e) =>{
+    showPage(1, e.target.textContent)
+    div.parentNode.removeChild(div)
+    appendPageLinks(1, parseInt(e.target.textContent, 10));
+//excellent links work, page reloads with proper content.
+   })
+//console.log(student_list.length+ " <-num students,  num pages-> " +numPages)
 }
-//removeElement(elementID)
-////   START HERE //////   THE ABOVE APPEARS TO BE WORKING Links work!
-//only the active href should have the "active" class applied
-
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-// function appendPageLinks(page){
-//   let parent = document.querySelector('.page')
-//   let div = document.createElement('div')
-//   div.className = "pagination"
-//   parent.appendChild(div);
-//   div.appendChild();
-//   let numPages = Math.ceil(student_list.length / number_of_items)
-// //alert(numPages)
-//     for(let i=1; i<=numPages; i++){
-//       let li = document.createElement('li');
-//       let span = document.createElement('span');
-//     //  span.innerHTML = '<a class="active" href="http:www.google.com">'+i+'</a>';//they work
-// //alert(numPages+ "   " + page)
-//       if(page === i){
-//         span.innerHTML = '<a class="active" href="#">'+i+'</a>';
-//         //span.addEventListener('click'){
-//           alert(li.a);
-//       //  }
-//       }
-//       else{
-//         span.innerHTML = '<a href="#">'+i+'</a>';
-//       }
-//       //span.innerHTML = '<a class="active" href="#">'+i+'</a>';
-//       li.appendChild(span);
-//       div.appendChild(li);
-//     }
-//
-//
-// console.log(student_list.length+ " " +numPages)
-// }
