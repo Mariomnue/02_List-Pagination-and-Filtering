6d996afcac4119cb0696c20b79c39f413d24448d
Mariomnue <mary.pienzi@gmail.com>
2020-05-13 10:46:14 -0400

    got it! Search works. only searching the names. Add key up!

--- js/script.js
+++ js/script.js
@@ -11,13 +11,8 @@ FSJS project 2 - List Filter and Pagination
   const number_of_items = 10;
   const divPagination = document.createElement('div')
   divPagination.className = "pagination"
-  //global variable that is necessary for the setSubset access.
-//  let newList = [];//find a way to put this inside the function
-  //newList is used by function listMaker to build the pagination list; no longer true;
-//  const list = student_list;//remove me later
   const showPage = (list, page) => {
     list = student_list;//remove me later
-    console.log(page);
     let startIndex = (page * number_of_items) - number_of_items;
     let endIndex = page * number_of_items;
       for(var i=0; i<student_list.length; i++){
@@ -28,14 +23,13 @@ FSJS project 2 - List Filter and Pagination
         }
       }
   }
-
   showPage(1, 1);///never send a 0;
   appendPageLinks(1);
 
 //below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
-  const reset= () => {
+    const reset= () => {
     divPagination.innerHTML = "";
-  }//borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
+    }//borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
 
   //the appendPageLinks function builds the navigation buttons at the bottom of the stage.
   function appendPageLinks(page){
@@ -56,16 +50,13 @@ FSJS project 2 - List Filter and Pagination
         li.appendChild(span);
         ul.appendChild(li);
       }
-
       ul.addEventListener('click', (e) =>{
       showPage(newlist, e.target.textContent)
       reset();
+      noResults.style.display = 'none';
       appendPageLinks(e.target.textContent);
      })
-
-
   }
-
 
 //  function searchForStudent(){
 //starts with building the search element and event
@@ -94,23 +85,18 @@ FSJS project 2 - List Filter and Pagination
     searchDiv.appendChild(noResults);
     noResults.style.display = 'none';
 //borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
-
     function searchForStudent(text){
-
-      // const nDiv = document.querySelector('.page');
-      // const ul = document.createElement('ul');
-      // ul.className = 'filtered_student-list'
-      // nDiv.appendChild(ul);
-
 //below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
       noResults.style.display = ('none');
       const filteredList = [];
       const searchString = text;//input from user
       for(var k=0; k<student_list.length; k++){
-        if(student_list[k].innerHTML.includes(input.value.toLowerCase())){//if any part of student contains the input value
+//console.log(student_list[k].firstElementChild.firstElementChild.nextSibling.nextSibling);
+      const student = student_list[k].firstElementChild.firstElementChild.nextSibling.nextSibling
+        if(student.innerHTML.includes(input.value.toLowerCase())){//if any part of student contains the input value
           student_list[k].style.display = ('');//show student;
           filteredList.push(student_list[k]);// push student to filtered list
-
+//console.log('before:  '+filteredList.length+ "student: " +student_list[k].innerHTML);
         }else{
           student_list[k].style.display = ('none');//else hide student;
         }
@@ -119,16 +105,15 @@ FSJS project 2 - List Filter and Pagination
       if (filteredList.length == 0){
         noResults.style.display = ('');
       }
-console.log(filteredList.length);
-
+console.log('after:  '+filteredList.length);
 reset();
-showPage(filteredList, 1);
 appendPageLinks(filteredList);
 //borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir saved me 12 lines of code that sorta worked.
 
 }
 
 form.addEventListener('submit', (e) => {
+  noResults.style.display = 'none';
   e.preventDefault();
   const text = input.value;
   searchForStudent(text);
