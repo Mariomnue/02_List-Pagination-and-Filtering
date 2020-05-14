b92146d415677c43b47b13763df7252b45264e7a
Mariomnue <mary.pienzi@gmail.com>
2020-05-14 13:39:49 -0400

    Almost done, list display, search display, pagination displays. When the search comes back with more than 10, the showPage doesn't work correcty.

--- js/script.js
+++ js/script.js
@@ -6,42 +6,67 @@ FSJS project 2 - List Filter and Pagination
 
 //document.addEventListener('DOMContentLoaded', () => {//
 ////REMB every list is a subset of this master list.
-////if the list ever comes from outside the html this will need to change
   const student_list = document.getElementsByClassName("student-item cf");
+  const filteredList = document.getElementsByClassName("student-item cf");//move to the top with description
+
   const number_of_items = 10;
   const divPagination = document.createElement('div')
-  divPagination.className = "pagination"
-  const showPage = (list, page) => {
-    list = student_list;//remove me later
-    let startIndex = (page * number_of_items) - number_of_items;
-    let endIndex = page * number_of_items;
+    divPagination.className = "pagination"
+  let target = "";
+  const showPage = (list, page) => {//start of showPage function
+  let startIndex = (page * number_of_items) - number_of_items;
+  let endIndex = page * number_of_items;
+
+    if(list >= 1){//number 1 is a filter for the list. if it doesn't exist or is the normal student_list then this number is 1.
       for(var i=0; i<student_list.length; i++){
         if(i >= startIndex && i < endIndex){
-          student_list[i].style.display = '';
+          student_list[i].style.display = ('');//if student is on this list then show them
         }else{
-          student_list[i].style.display = "none"
+          student_list[i].style.display = ("none")//else hide
         }
       }
+      appendPageLinks(student_list);
+    }
+    if(page == -2){//the page number here (-2) is coming from the searchForStudent function.
+      for(var i=0; i<filteredList.length; i++){
+        if(i >= startIndex && i < endIndex){
+          filteredList[i].style.display = ('');//if student is on this list then show them
+        }else{
+          filteredList[i].style.display = ("none")//else hide
+        }
+      }
+      appendPageLinks(filteredList);
+    }
+
   }
-  showPage(1, 1);///never send a 0;
-  appendPageLinks(1);
 
-//below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
+
+
+//below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.  This has undergone change from brunomarchir original
     const reset= () => {
-    divPagination.innerHTML = "";
-    }//borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
+      divPagination.innerHTML = "";
+    }
 
-  //the appendPageLinks function builds the navigation buttons at the bottom of the stage.
-  function appendPageLinks(page){
-      const newlist = document.querySelector('.page')//isn't this the same as student_list it's an HTML Collection
-      newlist.appendChild(divPagination)
+//the appendPageLinks function builds the navigation buttons at the bottom of the stage.
+//This function is called from showPage function only.
+  function appendPageLinks(list){////This should be a list not a page number
+//let list = list;
+console.log('appendPageLinks List.length: '+list.length);
+//divParent isn't the same as student_list it's an HTML Collection
+//it is used to place the paginatioin links at the bottom.
+      const divParent = document.querySelector('.page')
+      divParent.appendChild(divPagination)
       const ul = document.createElement('ul');
       divPagination.appendChild(ul);
-      let numPages = Math.ceil(student_list.length / number_of_items)
+      let numPages = Math.ceil(list.length / number_of_items)
+
       for(let i=1; i<=numPages; i++){
         let li = document.createElement('li');
         let span = document.createElement('span');////remove
-          if(i == page){//two == not three ===?
+          if(target === ""){//used to catch the error that is thrown.
+            target = 1;
+          }
+          if(i == target){
             span.innerHTML = '<a class="active" href="#">'+i+'</a>';
           }
           else{
@@ -50,11 +75,13 @@ FSJS project 2 - List Filter and Pagination
         li.appendChild(span);
         ul.appendChild(li);
       }
-      ul.addEventListener('click', (e) =>{
-      showPage(newlist, e.target.textContent)
-      reset();
-      noResults.style.display = 'none';
-      appendPageLinks(e.target.textContent);
+//######## DON'T TOUCH THIS
+//########  append the clickies to the pagination buttons
+      ul.addEventListener('click', (e) =>{//This is appending the links
+        reset();
+        target = e.target.textContent;
+        showPage(1, e.target.textContent)
+        noResults.style.display = 'none';
      })
   }
 
@@ -78,44 +105,51 @@ FSJS project 2 - List Filter and Pagination
     searchDiv.appendChild(form);
 
 //Display no Results when needed.
-//below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
+//below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.  This has undergone change from brunomarchir original
     //create 'no results' message
     noResults = document.createElement('h2');
     noResults.textContent = "Sorry, no results have been found.";
     searchDiv.appendChild(noResults);
-    noResults.style.display = 'none';
+    noResults.style.display = ('none');
 //borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
+
+
+
     function searchForStudent(text){
-//below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.
+//below borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir.  This has undergone change from brunomarchir original
+      let inputVal = text;
+      let cnt = 0; //count the number of matches;
       noResults.style.display = ('none');
-      const filteredList = [];
       const searchString = text;//input from user
-      for(var k=0; k<student_list.length; k++){
-//console.log(student_list[k].firstElementChild.firstElementChild.nextSibling.nextSibling);
-      const student = student_list[k].firstElementChild.firstElementChild.nextSibling.nextSibling
-        if(student.innerHTML.includes(input.value.toLowerCase())){//if any part of student contains the input value
-          student_list[k].style.display = ('');//show student;
-          filteredList.push(student_list[k]);// push student to filtered list
-//console.log('before:  '+filteredList.length+ "student: " +student_list[k].innerHTML);
-        }else{
-          student_list[k].style.display = ('none');//else hide student;
+
+      for(var k=0; k<filteredList.length; k++){
+        const student = filteredList[k].firstElementChild.firstElementChild.nextSibling.nextSibling///this targets the h3 element
+
+          if(student.innerHTML.includes(inputVal.toLowerCase())){//if any part of student contains the input value
+            //filteredList[k] =
+            filteredList[k].style.display = ('block');//show student;
+            cnt+=1;
+
+          }else{
+            filteredList[k].style.display = ("none");//else hide student;
+          }
         }
-
-      }
-      if (filteredList.length == 0){
+      if(cnt < 1){
         noResults.style.display = ('');
+      }else if(cnt > 0 && cnt < 10){
+        reset();//remove the pagination
       }
-console.log('after:  '+filteredList.length);
-reset();
-appendPageLinks(filteredList);
-//borrowed from brunomarchir/list-pagination-and-filtering// thanks brunomarchir saved me 12 lines of code that sorta worked.
-
-}
+      else{
+        showPage(filteredList, -2);//filter this list to only show needed ones
+      }
+    }
 
 form.addEventListener('submit', (e) => {
   noResults.style.display = 'none';
   e.preventDefault();
   const text = input.value;
-  searchForStudent(text);
   input.value = '';//clear it out after its been used.
+  searchForStudent(text);
 });
+
+showPage(1, 1);///never send a 0;
